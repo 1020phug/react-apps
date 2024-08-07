e7865241f26222c7c99e75d8df601af66810e8af
Phùng Quang Anh <tenzovn@gmail.com>
2024-08-07 15:31:09 +0700

    Initial Setup

---
+++ eat-n-split/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+	const initialFriends = [
+		{
+			id: 118836,
+			name: "Clark",
+			image: "https://i.pravatar.cc/48?u=118836",
+			balance: -7,
+		},
+		{
+			id: 933372,
+			name: "Sarah",
+			image: "https://i.pravatar.cc/48?u=933372",
+			balance: 20,
+		},
+		{
+			id: 499476,
+			name: "Anthony",
+			image: "https://i.pravatar.cc/48?u=499476",
+			balance: 0,
+		},
+	];
+	return <>{initialFriends.toString()}</>;
+}
+
+export default App;
